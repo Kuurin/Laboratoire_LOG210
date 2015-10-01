797b60dd36cf2059b30009f1937001de3d4b9720
oguevremont <guevremont.o@gmail.com>
2015-09-30 23:46:25 -0400

    ajout du field phone number au form étudiant

--- EnvVirtuel/src/projet_log210/forms.py
+++ EnvVirtuel/src/projet_log210/forms.py
@@ -4,16 +4,32 @@ from django.contrib.auth.forms import UserCreationForm
 
 class EtudiantRegistrationForm(UserCreationForm):
 	email = forms.EmailField(required=True)
+	phone = forms.CharField()
 	
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password1', 'password2')
+		fields = ('username', 'email', 'phone', 'password1', 'password2')
+	
+	#validation phone number	
+	def clean_phone(self):
+		value = self.cleaned_data['phone']
+		for char in value:
+			if not char.isdigit():
+				char.remove()
+		if len(value) is not 10:
+			raise forms.ValidationError("Votre numéro doit être écrit avec 10 chiffres")
+		return value
+		
+		
 	
 	def save(self,  commit=True):
 		user = super(EtudiantRegistrationForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
+		user.phone = self.cleaned_data['phone']
 		
 		if commit:
 			user.save()
 			
 		return user
+
+		
--- EnvVirtuel/src/projet_log210/settings.py
+++ EnvVirtuel/src/projet_log210/settings.py
@@ -109,3 +109,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#phone number
+PHONENUMBER_DB_FORMAT= 'INTERNATIONAL'
